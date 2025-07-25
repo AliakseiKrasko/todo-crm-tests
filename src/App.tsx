1851feb89b96432b600469b8054658bf3355d4ad
Aliaksei Krasko <alekskrasko1985@gmail.com>
2025-07-25 20:34:39 +0300

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export default function App() {
+    return (
+        <div className="p-8 bg-yellow-100 text-blue-600 text-3xl font-bold">
+            ✅ Tailwind работает!
+        </div>
+    );
+}
