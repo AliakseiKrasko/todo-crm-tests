f27ab2059c085332a6e39d3cb7370eeeb1f94ae7
Aliaksei Krasko <alekskrasko1985@gmail.com>
2025-07-28 18:06:07 +0300

    added cypress test all TodoList

--- cypress/e2e/todo.cy.ts
+++ cypress/e2e/todo.cy.ts
@@ -1,8 +1,37 @@
 describe('Todo E2E', () => {
-    it('Добавляет задачу через UI', () => {
-        cy.visit('http://localhost:5173/')
-        cy.get('input[placeholder="Введите задачу"]').type('Купить хлеб')
+    beforeEach(() => {
+        cy.visit('http://localhost:5173/');
+        cy.clearLocalStorage();
+    })
+    it('Added task to UI', () => {
+        cy.get('input[placeholder="Введите задачу"]').type('Купить молоко')
         cy.contains('Добавить').click()
-        cy.contains('Купить хлеб').should('exist')
+        cy.contains('Купить молоко').should('exist')
+    })
+
+
+    it('Remove task', () => {
+        cy.get('input[placeholder="Введите задачу"]').type('удалить меня')
+        cy.contains('Добавить').click()
+        cy.contains('удалить меня')
+            .parent()
+            .find('button')
+            .click()
+        cy.contains('Удалить меня').should('not.exist')
+    })
+
+    it('Marks the task as completed', () => {
+        cy.get('input[placeholder="Введите задачу"]').type('task to completed')
+        cy.contains('Добавить').click()
+        cy.contains('task to completed').click()
+        cy.contains('task to completed').should('have.class', 'line-through')
+    })
+
+    it('Displays the message “No tasks” if there are no tasks.', () => {
+        cy.window().then((win: Window) => {
+            win.localStorage.setItem('todos', '[]');
+        });
+        cy.reload();
+        cy.contains('Задач нет').should('exist');
     })
 })
