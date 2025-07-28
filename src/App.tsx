94c40aa204eca76b606fb4b393fd272975143464
Aliaksei Krasko <alekskrasko1985@gmail.com>
2025-07-28 10:30:55 +0300

    added react-router-dom and page transition

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,41 @@
 import AddTodo from './components/AddTodo';
 import TodoList from './components/TodoList';
+import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
+
+
+
+function CompletedPage() {
+    return (
+        <div>
+            <h2 className="text-xl font-bold mb-4">Выполненные задачи</h2>
+            <TodoList onlyCompleted />
+            <Link to="/" className="text-blue-500">⬅️ Все задачи</Link>
+        </div>
+    )
+}
+
+function HomePage() {
+    return (
+        <div>
+            <h1 className="text-2xl font-bold mb-4">Все задачи</h1>
+            <AddTodo />
+            <TodoList />
+            <Link to="/completed" className="text-blue-500">Только выполненные</Link>
+        </div>
+    )
+}
+
 
 export default function App() {
     return (
-        <div className="max-w-xl mx-auto mt-10 bg-gray-50 p-8 rounded shadow">
-            <h1 className="text-2xl font-bold mb-6 text-center">Todo CRM</h1>
-            <AddTodo />
-            <TodoList />
-        </div>
+        <BrowserRouter>
+            <div className="max-w-xl mx-auto mt-10 bg-gray-50 p-8 rounded shadow">
+                <Routes>
+                    <Route path="/" element={<HomePage />} />
+                    <Route path="/completed" element={<CompletedPage />} />
+                </Routes>
+            </div>
+        </BrowserRouter>
+
     );
 }
